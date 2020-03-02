720917a4e7a6b294152193014890e4b4cd3ad9aa
Feng Lyu <fenglyu007@gmail.com>
2020-03-03 00:10:10 +0800

    update

--- cmd/main.go
+++ cmd/main.go
@@ -11,5 +11,15 @@ import (
 
 func main() {
 	a := smbios.NewDMITable()
+	fmt.Println(a.Version())
+	//
 	fmt.Println(a.GetEntriesByType(17))
+
+	//	for i, v := range smbios.StringKeyword {
+	//		fmt.Printf("[%d] Keyword: %s, Type: %d, Offset: %d\n", i, v.Keyword, v.Type, v.Offset)
+	//	}
+	//
+	for k, v := range a.Mapping {
+		fmt.Printf("key: %s, { Keyword: %s, Type: %d, Offset: %d }\n", k, v.Keyword, v.Type, v.Offset)
+	}
 }
--- smbios.go
+++ smbios.go
@@ -7,10 +7,44 @@ import (
 	"github.com/digitalocean/go-smbios/smbios"
 )
 
+type StringKW struct {
+	Keyword string
+	Type    uint8
+	Offset  uint8
+}
+
+var string_keyword = []*StringKW{
+	{"bios-vendor", 0, 0x04},
+	{"bios-version", 0, 0x05},
+	{"bios-release-date", 0, 0x08},
+	{"bios-revision", 0, 0x15},     /* 0x14 and 0x15 */
+	{"firmware-revision", 0, 0x17}, /* 0x16 and 0x17 */
+	{"system-manufacturer", 1, 0x04},
+	{"system-product-name", 1, 0x05},
+	{"system-version", 1, 0x06},
+	{"system-serial-number", 1, 0x07},
+	{"system-uuid", 1, 0x08}, /* dmi_system_uuid() */
+	{"system-family", 1, 0x1a},
+	{"baseboard-manufacturer", 2, 0x04},
+	{"baseboard-product-name", 2, 0x05},
+	{"baseboard-version", 2, 0x06},
+	{"baseboard-serial-number", 2, 0x07},
+	{"baseboard-asset-tag", 2, 0x08},
+	{"chassis-manufacturer", 3, 0x04},
+	{"chassis-type", 3, 0x05}, /* dmi_chassis_type() */
+	{"chassis-version", 3, 0x06},
+	{"chassis-serial-number", 3, 0x07},
+	{"chassis-asset-tag", 3, 0x08},
+	{"processor-family", 4, 0x06}, /* dmi_processor_family() */
+	{"processor-manufacturer", 4, 0x07},
+	{"processor-version", 4, 0x10},
+	{"processor-frequency", 4, 0x16}, /* dmi_processor_frequency() */
+}
+
 type DMIType uint8
 
 type DMITable struct {
-	mapping map[uint8]*smbios.Structure
+	Mapping map[string]*StringKW
 	ep      smbios.EntryPoint
 	ss      []*smbios.Structure
 }
@@ -33,6 +67,12 @@ func NewDMITable() *DMITable {
 	}
 	dt.ep = ep
 	dt.ss = ss
+
+	mapping := make(map[string]*StringKW)
+	for _, v := range string_keyword {
+		mapping[v.Keyword] = v
+	}
+	dt.Mapping = mapping
 	return dt
 }
 
@@ -45,10 +85,17 @@ func (dmit *DMITable) Version() string {
 		major, minor, rev, addr, size)
 }
 
+//func (dmit *DMITable)
+
 /*
 bios-vendor, bios-ver‐sion, bios-release-date, system-manufacturer, system-product-name, system-version, system-serial-number, system-uuid, baseboard-manufacturer,baseboard-product-name, baseboard-version, baseboard-serial-number, baseboard-asset-tag, chassis-manufacturer, chassis-type, chassis-version, chassis-serial-number, chassis-asset-tag, processor-family, processor-manufacturer,  processor-version,  processor-frequency.
 */
-func (dmit *DMITable) GetResultByKeyword(keyword string) interface{} {
+func (dmit *DMITable) GetResultByKeyword(keyword string) string {
+
+	if _, ok := dmit.Mapping[keyword]; ok == nil {
+		return ""
+	}
+
 	switch keyword {
 	case "bios-vendor":
 		fmt.Println(keyword)
@@ -60,17 +107,10 @@ func (dmit *DMITable) GetResultByKeyword(keyword string) interface{} {
 		fmt.Println(keyword)
 	}
 
-	return nil
+	return ""
 }
 
 func (dmit *DMITable) GetEntriesByType(et DMIType) string {
-
-	major, minor, rev := dmit.ep.Version()
-	addr, size := dmit.ep.Table()
-
-	fmt.Printf("SMBIOS %d.%d.%d - table: address: %#x, size: %d\n",
-		major, minor, rev, addr, size)
-
 	for _, s := range dmit.ss {
 		//	fmt.Printf("---> %d\n", s.Header.Type)
 		if s.Header.Type == uint8(et) {
